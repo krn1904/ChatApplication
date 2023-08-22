5fce025fe94833af784e12e0bc19f3fd6b603bed
Karan Soni <Karanssoni2002@gmail.com>
2023-08-22 17:15:10 +0530

    structuring the files

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,17 +1,47 @@
-import './Main.css';
+import React, { useState } from "react";
+import "./Main.css";
+import io from "socket.io-client";
+import TopNavBar from "../TopnavBar/TopNavBar";
 
 function Main() {
-  return( 
-    <div className="background">
-      <div className='messages'>
-      Display the text messages
+  const [messageInput, setMessageInput] = useState("");
+  const [submittedMessage, setSubmittedMessage] = useState("");
+
+  // Event handler to update the message input
+  const handleInputChange = (event) => {
+    setMessageInput(event.target.value);
+  };
+
+  // Event handler for submitting the message
+  const handleSubmit = () => {
+    setSubmittedMessage(messageInput);
+    setMessageInput("");
+  };
+
+  return (
+    <>
+      <TopNavBar />
+      <div className="background">
+        <div className="message_background">
+          {submittedMessage && (
+            <div className="Message">{submittedMessage}</div>
+          )}
+        </div>
+        <div className="inputfield">
+          <input
+            className="inputbox"
+            type="text"
+            placeholder="Please enter your text"
+            value={messageInput} // Bind input value to the messageInput state
+            onChange={handleInputChange}
+          />
+          <button className="sendButton" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
       </div>
-      <div className='inputfield'>
-        <input className='inputbox' type='text' placeholder='Please enter your text'/>
-        <button className='sendButton'>Submit</button>
-      </div>
-    </div>
-    );
+    </>
+  );
 }
 
 export default Main;
