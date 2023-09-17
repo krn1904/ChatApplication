502945c2f0419e2bf4707f21ee746b108e098154
Karan Soni <Karanssoni2002@gmail.com>
2023-09-17 23:01:15 +0530

    update structure

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,17 +1,52 @@
 import "./Styles/App.css";
 import Main from "./Components/Main/Main";
-import LoginPage from "./Components/LoginPage/LoginPage";
-import { BrowserRouter, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { io } from "socket.io-client";
+import { useState } from "react";
+const Config = require('./config.json');
+
+const socket = io.connect(Config.BaseURL + Config.port, { transports: ["websocket"] });
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <LoginPage/> }/>
-        <Route path="/Home" element={ <Main/> }/>
-      </Routes>
-    </BrowserRouter>
-  );
+    const [username, setUsername] = useState("");
+    const [room, setRoom] = useState("");
+    const [showChat, setShowChat] = useState(false);
+  
+    const joinRoom = () => {
+      if (room !== "" && username !== "") {
+        socket.emit("join_room", room);
+      }
+      setShowChat(true);
+    };
+  
+    return (
+      <>
+      {!showChat ?
+        <div className="join_room">
+          <div className="login-form">
+            <div className="input-container">
+              <label>username:</label>
+              <input
+                type="text"
+                placeholder="krn"
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </div>
+            <div className="input-container">
+              <label>RoomId:</label>
+              <input
+                type="text"
+                placeholder="Enter Room id"
+                onChange={(event) => setRoom(event.target.value)}
+              />
+            </div>
+  
+            <button onClick={joinRoom}>Join Room</button>
+          </div>
+        </div>
+        : <Main room={room} username={username} socket={socket} />
+      }
+      </>
+    );
 }
 
 export default App;
