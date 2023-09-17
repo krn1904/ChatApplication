b6b0af681706d1e8c2831954124d4e826bc7f600
Karan Soni <Karanssoni2002@gmail.com>
2023-09-17 23:00:46 +0530

    update structure

--- FE/src/Components/Main/Main.js
+++ FE/src/Components/Main/Main.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Main.css";
-import io from "socket.io-client";
 import TopNavBar from "../TopnavBar/TopNavBar";
 
-function Main() {
+function Main({ room, username, socket }) {
   const [messageInput, setMessageInput] = useState("");
-  const [submittedMessage, setSubmittedMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
   // Event handler to update the message input
   const handleInputChange = (event) => {
@@ -13,19 +12,56 @@ function Main() {
   };
 
   // Event handler for submitting the message
-  const handleSubmit = () => {
-    setSubmittedMessage(messageInput);
-    setMessageInput("");
+  const handleSubmit = async () => {
+    if (messageInput !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: messageInput,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+    }
   };
+
+  useEffect(() => {
+    socket.on("recieve_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+    // Return a cleanup function to remove the event listener when the component unmounts
+    return () => {
+      socket.off("recieve_message");
+    };
+  }, [socket, messageList]);
 
   return (
     <>
       <TopNavBar />
       <div className="background">
         <div className="message_background">
-          {submittedMessage && (
-            <div className="Message">{submittedMessage}</div>
-          )}
+          {messageList.map((messageContent, index) => {
+            return (
+              <div
+                className="Message"
+                key={index}
+                id={username == messageContent.author ? "You" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p>{messageContent.time}</p>
+                    <p>{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
         <div className="inputfield">
           <input
