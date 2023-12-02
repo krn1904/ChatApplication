8b97e11f0c2f111d6ca3fec633fcc9fe7f3aa639
Karan Soni <Karanssoni2002@gmail.com>
2023-12-02 23:31:07 +0530

    temp changes

--- FE/src/config.js
+++ FE/src/config.js
@@ -1,7 +1,7 @@
-require("dotenv").config()
+// require("dotenv").config()
 const config = {
     port : process.env.port || ':3001',
-    BaseURL : process.env.BaseURL ,
+    BaseURL : process.env.BaseURL || "https://chatapp-backend-fa2x.onrender.com",
 }
 
 module.exports = config
