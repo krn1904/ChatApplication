10216d5ef5d9ba92c1fa7f25a3e6eb34d91ec43d
Karan Soni <Karanssoni2002@gmail.com>
2023-12-02 23:21:40 +0530

    temp fix

--- FE/src/config.js
+++ FE/src/config.js
@@ -1,6 +1,7 @@
+require("dotenv").config()
 const config = {
     port : process.env.port || ':3001',
-    BaseURL : process.env.BaseURL,
+    BaseURL : process.env.BaseURL ,
 }
 
 module.exports = config
