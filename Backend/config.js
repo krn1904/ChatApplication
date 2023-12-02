5fd80c52feef2991ab623990822cd82f9d0f69d4
Karan Soni <Karanssoni2002@gmail.com>
2023-12-02 21:42:36 +0530

    Added process.env for config

---
+++ Backend/config.js
@@ -0,0 +1,5 @@
+const config = {
+ MongoDb_URL: process.env.MongoDb_URL || 'Add Mongo URL'
+};
+// export default config;
+module.exports = config;
---
+++ FE/src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    port : process.env.port || ':3001',
+    BaseURL : process.env.BaseURL || 'http://192.168.1.10'
+}
+
+module.exports = config
