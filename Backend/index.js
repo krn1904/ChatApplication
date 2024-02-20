da1b3110dd745ecb476918a0b2b408d869f76dd4
Karan Soni <Karanssoni2002@gmail.com>
2024-02-20 10:06:01 +0530

    websocket integrated

--- Backend/index.js
+++ Backend/index.js
@@ -1,100 +1,56 @@
 const express = require('express');
 const  http  = require("http");
-// const  createServer  = require("http");
-// const mongoose = require('mongoose');
-// const { Server } = require("socket.io"); 
-const { WebSocketServer } = require("ws");
-const cors = require('cors') // Import the cors middleware
-const { CreateUser, AllUsers } = require('./UserController/Users');
-const config = require('./config.js');
+const { WebSocket } = require("ws");
+const { handleMessage } = require('./Websocket/ws');
 
-// const app = express();
-// const httpServer = createServer(app);
-// // const io = new Server(httpServer)
-// const io = new WebSocketServer({server : httpServer});
-// const port = 3002;
-
-
-// app.use(cors()); // Use the cors middleware
-// app.use(express.json());
-
-
-// // //handle conncection
-// io.on('connection', (socket) => {
-//   console.log(`User Connected: ${socket.id}`);
-
-  // Handle custom events and messages here
-
-//   socket.on('join_room', (data) => {
-//     socket.join(data);
-//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
-//   });
-
-//   socket.on('send_message', (data)=>{
-//     socket.to(data.room).emit('recieve_message',data)
-//     console.log("in send_msg",data)
-//   })
-
-//   socket.on('disconnect', () => {
-//     console.log('A user disconnected');
-//   });
-
-
-// });
-
-//connect to the database
-// async function startServer() {
-//   await connect();
-  
-//   httpServer.listen(port, () => {
-//     console.log(`Server is listening at http://localhost:${port}`);
-//   });
-// }
-
-//API 
-// app.post('/createUser',CreateUser)
-// app.get('/AllUsers',AllUsers)
-
-
-// async function connect() {
-//   try {
-//     console.log(config)
-//     await mongoose.connect(config.MongoDb_URL, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('Connected to MongoDB');
-//   } catch (error) {
-//     console.error('MongoDB connection error:', error);
-//   }
-// }
-
-// startServer();
-
-
-// Shift it to websocket
-
+// Code changed to websocket
 const app = express();
 
-const host = process.env.HOST || "192.168.1.11";
-const port = process.env.PORT || 8000;
+const host = process.env.HOST || "192.168.1.3";
+const port = process.env.PORT || 8001;
 const server = http.createServer(app);
-const wss = new WebSocketServer({server : server});
-let wsConnection;
+const wss = new WebSocket.Server({port : 8002});
 let clients = [];
 
 app.use(express.json()); // json body parser
 
-wss.on("connection",(ws)=>{
-  console.log(ws)
-  ws.on("open",(data)=>{
-    console.log("connection established")
+wss.on("connection",(ws) => initConnection(ws))
+
+function initConnection (ws) {
+  // NEW CODE
+  clients.push(ws);
+  // console.log("clients", clients)
+  ws.on('message', async (data) => {
+    try {
+      let req = JSON.parse(data);
+      // console.log("req",JSON.parse(data))
+      // console.log("ws",ws)
+      console.log("outside ws",ws)
+      let res = await handleMessage(req, ws)
+      console.log("res",res)
+    // res.forEach(element => {
+      // res[0].element.websocketConnection.send("data")
+    // });
+        // ws.send(res)
+        // res.send("Hey")
+
+      // console.log(res)
+      // console.log("clients",clients)
+      // res.forEach((client) => {
+      //   console.log("inside clients", client)
+      //   let websocket = client.websocketConnection
+      //   console.log(websocket)
+      //   // websocket.send()
+      // });
+      
+
+    } catch (error) {
+      console.error('Error parsing WebSocket message:', error);
+    }
   })
-  ws.on("open",(data)=>{
-    console.log("connection closed")
-  })
-})
+}
 
 server.listen(port, () => {
   console.log(`Server running at http://${host}:${port}`);
+  console.log("HI")
 });
